b93f9b56643c2393c5723af1f9d7e95b0ddf2326
Pablo Moreira De Sousa <pmoreira@c1r1s9.42lisboa.com>
2024-10-22 16:31:09 +0100

    ex15

---
+++ ex15/ft_putstr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 16:25:13 by pmoreira          #+#    #+#             */
+/*   Updated: 2024/10/22 16:30:52 by pmoreira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != 0)
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+/*
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int main(void)
+{
+	char *str = "teste";
+	ft_putstr(str);
+}
+*/
