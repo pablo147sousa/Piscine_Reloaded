37daddf57b1014a2a87c6753f51ba46f9f6acd6e
Pablo Moreira De Sousa <pmoreira@c1r1s9.42lisboa.com>
2024-10-22 14:40:27 +0100

    ex08

---
+++ ex08/ft_is_negative.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 14:23:49 by pmoreira          #+#    #+#             */
+/*   Updated: 2024/10/22 14:39:56 by pmoreira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_is_negative(int n);
+
+void	ft_is_negative(int n)
+{
+	if (n < 0)
+		ft_putchar('N');
+	else
+		ft_putchar('P');
+}
+/*
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int main(void)
+{
+	ft_is_negative(0);
+}
+*/
