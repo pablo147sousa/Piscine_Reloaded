b2e2b1d196594438738bbc68d934a1196117ab08
Pablo Moreira De Sousa <pmoreira@c1r1s9.42lisboa.com>
2024-10-22 14:58:54 +0100

    ex11

---
+++ ex11/ft_div_mod.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 14:50:54 by pmoreira          #+#    #+#             */
+/*   Updated: 2024/10/22 14:58:20 by pmoreira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_div_mod(int a, int b, int *div, int *mod);
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+/*
+#include <stdio.h>
+
+int main()
+{
+	int	div;
+	int	mod;
+	ft_div_mod(8, 3, &div, &mod);
+	printf("depois : %d e %d", div, mod);
+	return (0);
+}
+*/
