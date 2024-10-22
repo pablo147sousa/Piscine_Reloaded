292f18ac17c245f374fc3d6177872e1c13b04571
Pablo Moreira De Sousa <pmoreira@c1r1s9.42lisboa.com>
2024-10-22 14:46:23 +0100

    ex09

---
+++ ex09/ft_ft.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 14:40:48 by pmoreira          #+#    #+#             */
+/*   Updated: 2024/10/22 14:45:40 by pmoreira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ft(int *nbr);
+
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+/*
+#include <stdio.h>
+
+int main()
+{
+	int	num;
+
+	num = 0;
+	printf("antes : %d", num);
+	ft_ft(&num);
+	printf("depois : %d", num);
+	return (0);
+}
+*/
