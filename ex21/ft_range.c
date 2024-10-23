79dcccf638030493ee4705511e9b8f5df3bceba8
Pablo Moreira De Sousa <pmoreira@c1r2s11.42lisboa.com>
2024-10-23 14:53:54 +0100

    ex21

---
+++ ex21/ft_range.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 14:33:29 by pmoreira          #+#    #+#             */
+/*   Updated: 2024/10/23 14:53:30 by pmoreira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_range(int min, int max);
+
+int	*ft_range(int min, int max)
+{
+	int	*range;
+	int	i;
+
+	i = 0;
+	if (min > max)
+		return (0);
+	range = (int *) malloc((max - min) * sizeof(int));
+	if (range == 0)
+		return (0);
+	while (i < (max - min))
+	{
+		range[i] = min + i;
+		i++;
+	}
+	return (range);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	i;
+	int	*range;
+	int	min;
+	int	max;
+
+	min = -3;
+	max = 7;
+	i = 0;
+	range = ft_range(min, max);
+	while (i < (max - min))
+	{
+		printf("%d\n", range[i]);
+		i++;
+	}
+}
+*/
