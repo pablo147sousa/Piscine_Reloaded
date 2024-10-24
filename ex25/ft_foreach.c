3a29345eaad9b9bb6cd55ff26469d2ea87ed9352
Pablo Moreira De Sousa <pmoreira@c1r2s9.42lisboa.com>
2024-10-24 15:13:35 +0100

    fix ex25

--- ex25/ft_foreach.c
+++ ex25/ft_foreach.c
@@ -6,7 +6,7 @@
 /*   By: pmoreira <pmoreira@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:59:37 by pmoreira          #+#    #+#             */
-/*   Updated: 2024/10/24 15:01:36 by pmoreira         ###   ########.fr       */
+/*   Updated: 2024/10/24 15:12:05 by pmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_foreach(int *tab, int length, void (*f)(int))
 	i = 0;
 	while (i < length)
 	{
-		f(i);
+		f(tab[i]);
 		i++;
 	}
 }
